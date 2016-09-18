c8550aa17966c11c90603b959ddb9a3dfb2068cd
Si Te Feng <fengsite@hotmail.com>
2016-09-18 11:39:07 -0400

    added h bridge code

--- arduino/hBridge/hBridge.ino
+++ arduino/hBridge/hBridge.ino
@@ -1,18 +1,26 @@
+#include <Stepper.h>
+
+#define STEPS 100
+
+Stepper stepper(STEPS, 8, 9, 10, 11);
+
 const int stepPin = 7;
 const int dirPin = 8;
 const int motorEnablePin1 = 9;
+const int motorEnablePin2 = 2;
 
+const int motor2PinA = 3;
+const int motor2PinB = 4;
+const int motor2PinC = 5;
+const int motor2PinD = 6;
+
+const int totalSteps = 380;
 const int incrementalSteps = 50;
 
 void setup() {
   Serial.begin(9600);
   
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
-  pinMode(motorEnablePin1, OUTPUT);
-
-  // LOW means ENABLED
-  digitalWrite(motorEnablePin1, LOW);
+  stepper.setSpeed(10);
 }
 
 void loop() {
@@ -24,20 +32,14 @@ void loop() {
     if (incomingByte == 97) {
       digitalWrite(dirPin, HIGH);
       for(int i=0; i< incrementalSteps; i++) {
-        digitalWrite(stepPin, HIGH);
-        delayMicroseconds(1000);
-        digitalWrite(stepPin, LOW);
-        delayMicroseconds(1000);
+        stepper.step(30);
       }
     }
     else if (incomingByte == 121) { // up
 
       digitalWrite(dirPin, LOW);
       for(int i=0; i< incrementalSteps; i++) {
-        digitalWrite(stepPin, HIGH);
-        delayMicroseconds(1000);
-        digitalWrite(stepPin, LOW);
-        delayMicroseconds(1000);
+        stepper.step(-30);
       }
    
     }
--- arduino/motorControl/motorControl.ino
+++ arduino/motorControl/motorControl.ino
@@ -1,12 +1,6 @@
 const int stepPin = 7;
 const int dirPin = 8;
 const int motorEnablePin1 = 9;
-const int motorEnablePin2 = 2;
-
-const int motor2PinA = 3;
-const int motor2PinB = 4;
-const int motor2PinC = 5;
-const int motor2PinD = 6;
 
 const int forcePin1 = 2;
 const int forcePin2 = 3;
@@ -19,77 +13,100 @@ int upCounter = 0;
 
 void setup() {
   Serial.begin(9600);
-  
+
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
   pinMode(motorEnablePin1, OUTPUT);
-  pinMode(motorEnablePin2, OUTPUT);
 
   pinMode(forcePin1, INPUT);
   pinMode(forcePin2, INPUT);
   pinMode(forcePin3, INPUT);
   pinMode(forcePin4, INPUT);
-  
+
+  pinMode(13, OUTPUT);
+
+
   // LOW means ENABLED
   digitalWrite(motorEnablePin1, LOW);
-  digitalWrite(motorEnablePin2, HIGH);
 }
 
 void loop() {
-  
 
   int val1 = digitalRead(forcePin1);
   int val2 = digitalRead(forcePin2);
   int val3 = digitalRead(forcePin3);
   int val4 = digitalRead(forcePin4);
+//
+//  if (val2 == 0) {
+//    digitalWrite(dirPin, HIGH);
+//    for (int i = 0; i < incrementalSteps; i++) {
+//      digitalWrite(stepPin, HIGH);
+//      delayMicroseconds(1000);
+//      digitalWrite(stepPin, LOW);
+//      delayMicroseconds(1000);
+//    }
+//  }
+//  else if (val2 == 1) { // up
+//
+//    digitalWrite(dirPin, LOW);
+//    for (int i = 0; i < incrementalSteps; i++) {
+//      digitalWrite(stepPin, HIGH);
+//      delayMicroseconds(1000);
+//      digitalWrite(stepPin, LOW);
+//      delayMicroseconds(1000);
+//    }
+//
+//  }
 
-  int shouldGoUp = 0;
-  int delayMicro = 1000;
-  if (val1 == 0) {
-    delayMicro = 1100;
-    
-    // HIGH means arm going down
-    digitalWrite(dirPin, HIGH);
-    if (upCounter > 0) {
+ 
+    int shouldGoUp = 0;
+    int delayMicro = 1000;
+    if (val1 == 0) {
+      delayMicro = 1100;
+
+      // HIGH means arm going down
+      digitalWrite(dirPin, HIGH);
+      if (upCounter > 0) {
+        digitalWrite(stepPin, HIGH);
+        delayMicroseconds(delayMicro);
+        digitalWrite(stepPin, LOW);
+        delayMicroseconds(delayMicro);
+
+        upCounter -= 1;
+      }
+      return;
+
+    } else if (val2 == 0) {
+
+      //do nothing
+      return;
+
+    } else if (val3 == 0) {
+      delayMicro = 3000;
+      shouldGoUp = 1;
+    } else if (val4 == 0) {
+      delayMicro = 2000;
+      shouldGoUp = 1;
+    } else if (val4 == 1) {
+      delayMicro = 1100;
+      shouldGoUp = 1;
+    }
+
+    if (shouldGoUp == 1) {
+      // Arm going up
+      digitalWrite(dirPin, LOW);
+
       digitalWrite(stepPin, HIGH);
       delayMicroseconds(delayMicro);
       digitalWrite(stepPin, LOW);
       delayMicroseconds(delayMicro);
-  
-      upCounter -= 1;
+      upCounter += 1;
+
+      //Serial.print("Pos: ");
+      //Serial.println(upCounter);
+      return;
     }
-    return;
 
-  } else if (val2 == 0) {
-
-    //do nothing
-    return;
-    
-  } else if (val3 == 0) {
-    delayMicro = 3000;
-    shouldGoUp = 1;
-  } else if (val4 == 0) {
-    delayMicro = 2000;
-    shouldGoUp = 1;
-  } else if (val4 == 1) {
-    delayMicro = 1100;
-    shouldGoUp = 1;
   }
-
-  if (shouldGoUp == 1) {
-    // Arm going up
-    digitalWrite(dirPin, LOW);
-    
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(delayMicro);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(delayMicro);
-    upCounter += 1;
-    
-    Serial.print("Pos: ");
-    Serial.println(upCounter);
-    return;
-  }
-  
+ 
 }
-
